c863a9006da218419fabb12964c16ad18784b4df
Frodo Lai <frodo_lai@bcmcom.com>
2014-01-06 20:31:23 -0800

    1. Remove AR6MXProductCommon.mk and move contents to ar6mx.mk 2. Add fstab.bcm to load partitions in init.freescale.rc

--- ar6mx/ar6mx.mk
+++ ar6mx/ar6mx.mk
@@ -7,5 +7,17 @@ $(call inherit-product-if-exists,vendor/google/products/gms.mk)
 # Overrides
 PRODUCT_NAME := ar6mx
 PRODUCT_DEVICE := ar6mx
+PRODUCT_BRAND := bcm
+PRODUCT_MANUFACTURER := bcm
 
-include device/bcm/ar6mx/AR6MXProductCommon.mk
+PRODUCT_COPY_FILES += \
+	device/bcm/ar6mx/required_hardware.xml:system/etc/permissions/required_hardware.xml \
+	device/bcm/ar6mx/init.rc:root/init.freescale.rc \
+	device/bcm/ar6mx/fstab.bcm:root/fstab.freescale \
+	device/bcm/ar6mx/vold.fstab:system/etc/vold.fstab \
+	device/bcm/ar6mx/gpsreset.sh:system/etc/gpsreset.sh
+
+# GPU files
+
+DEVICE_PACKAGE_OVERLAYS := device/bcm/ar6mx/overlay
+
