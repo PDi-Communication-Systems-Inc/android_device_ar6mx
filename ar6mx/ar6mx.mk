c5cc88295b2b03ce60f46faec389a918db46a6ee
Frodo Lai <frodo_lai@bcmcom.com>
2014-01-09 20:56:36 -0800

    Copy default settings from sabresd_6dq. Use virtual sdcard daemon to mount extsd instead of vold.fstab.

--- ar6mx/ar6mx.mk
+++ ar6mx/ar6mx.mk
@@ -14,10 +14,35 @@ PRODUCT_COPY_FILES += \
 	device/bcm/ar6mx/required_hardware.xml:system/etc/permissions/required_hardware.xml \
 	device/bcm/ar6mx/init.rc:root/init.freescale.rc \
 	device/bcm/ar6mx/fstab.bcm:root/fstab.freescale \
-	device/bcm/ar6mx/vold.fstab:system/etc/vold.fstab \
-	device/bcm/ar6mx/gpsreset.sh:system/etc/gpsreset.sh
+	device/bcm/ar6mx/gpsreset.sh:system/etc/gpsreset.sh \
+	device/fsl/sabresd_6dq/audio_policy.conf:system/etc/audio_policy.conf \
+	device/fsl/sabresd_6dq/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
 # GPU files
 
 DEVICE_PACKAGE_OVERLAYS := device/bcm/ar6mx/overlay
 
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_AAPT_CONFIG += xlarge large tvdpi hdpi
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.faketouch.xml:system/etc/permissions/android.hardware.faketouch.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+# for PDK build, include only when the dir exists
+# too early to use $(TARGET_BUILD_PDK)
+ifneq ($(wildcard packages/wallpapers/LivePicker),)
+PRODUCT_COPY_FILES += \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+endif
+
+PRODUCT_PACKAGES += AudioRoute
