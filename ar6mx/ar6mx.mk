8a5e7a7d3137f688f6b766bc78bcd6509a6f41ee
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2014-06-12 15:05:15 -0400

    Changes to make the Intel 3160 work

--- ar6mx/ar6mx.mk
+++ ar6mx/ar6mx.mk
@@ -19,7 +19,8 @@ PRODUCT_COPY_FILES += \
 	device/bcm/EETI/eGalaxTouch_VirtualDevice.idc:system/usr/idc/eGalaxTouch_VirtualDevice.idc \
         device/bcm/EETI/eGTouchA.ini:data/eGTouchA.ini \
         device/bcm/EETI/eGTouchD:system/bin/eGTouchD \
-	device/bcm/ar6mx/process_ts.sh:system/etc/process_ts.sh
+	device/bcm/ar6mx/process_ts.sh:system/etc/process_ts.sh \
+	device/bcm/ar6mx/load_wifi_module.sh:system/etc/load_wifi_module.sh
 
 # GPU files
 
