19ccc4fe3a480eee6fb2d63f884305d4ddcb1ae6
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2015-04-03 09:10:05 -0400

    Reliabilty improvements when working with the Atmel MXT1664S1 touchscreen at boot

--- ar6mx/process_ts.sh
+++ ar6mx/process_ts.sh
@@ -5,9 +5,40 @@ if [ $COUNT -ge "1" ];
   then
       echo "$COUNT Atmel Touchscreen discovered"
       setprop pdiarm.touchscreen Atmel
-      getprop pdiarm.touchscreen
       mxt-app -i
+      RESULT=$?	
+      let COUNTER=0
+      if [ $RESULT -gt "0" ];
+         then
+         while [ $COUNTER -lt 2 ]; do
+            echo "Retrying mxt-app, try $COUNTER"
+            mxt-app -i
+            RESULT=$?
+            if [ $RESULT -eq "0" ]; 
+               then
+               let COUNTER=100
+            else
+	       let COUNTER=COUNTER+1
+	    fi
+         done
+      fi
       mxt-app --reset
+      RESULT=$? 
+      COUNTER=0
+      if [ $RESULT -gt "0" ];
+         then
+         while [ $COUNTER -lt 2 ]; do
+            echo "Retrying mxt-app, try $COUNTER"
+            mxt-app --reset
+            RESULT=$?
+            if [ $RESULT -eq "0" ]; 
+               then
+               let COUNTER=100
+            else
+               let COUNTER=COUNTER+1
+            fi
+         done
+      fi
       PROCESS=`ps | grep eGTouchD | busybox tr -s " " | busybox cut -d " " -f 2`
       if [ $PROCESS -gt "0" ];
          then
@@ -16,7 +47,6 @@ if [ $COUNT -ge "1" ];
       else
           echo "Nothing to kill"
       fi
-      setprop pdiarm.touchscreen 
 fi
 COUNT=`lsusb | grep -i egalax | busybox wc -l`
 if [ $COUNT -ge "1" ];
@@ -24,4 +54,5 @@ if [ $COUNT -ge "1" ];
        echo "$COUNT eGalax Touchscreen discovered"
        setprop pdiarm.touchscreen eGalax
 fi
-echo "Done with touchscreen processing"
+PROP="$(getprop pdiarm.touchscreen)"
+echo "Done with touchscreen processing $PROP"
