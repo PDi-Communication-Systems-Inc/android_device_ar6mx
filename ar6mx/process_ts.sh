2b2b6798207d3c007a4ac0f25f6d45ad8ed5557e
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2015-04-03 10:14:33 -0400

    Simplify and add use of usbreset to simulate reinserting the usb header cable into CN5

--- ar6mx/process_ts.sh
+++ ar6mx/process_ts.sh
@@ -1,44 +1,42 @@
 #!/system/bin/sh
 
+# Test for Atmel or EETI touchscreen
 COUNT=`lsusb | grep -i atmel | busybox wc -l`
+
+# Atmel touchscreen handling
 if [ $COUNT -ge "1" ];
   then
+      # try to reset Atmel MXT1664S1 up to five times
       echo "$COUNT Atmel Touchscreen discovered"
       setprop pdiarm.touchscreen Atmel
-      mxt-app -i
+      mxt-app --reset
       RESULT=$?	
       let COUNTER=0
       if [ $RESULT -gt "0" ];
          then
-         while [ $COUNTER -lt 2 ]; do
-            echo "Retrying mxt-app, try $COUNTER"
-            mxt-app -i
-            RESULT=$?
-            if [ $RESULT -eq "0" ]; 
-               then
-               let COUNTER=100
-            else
-	       let COUNTER=COUNTER+1
-	    fi
-         done
-      fi
-      mxt-app --reset
-      RESULT=$? 
-      COUNTER=0
-      if [ $RESULT -gt "0" ];
-         then
-         while [ $COUNTER -lt 2 ]; do
-            echo "Retrying mxt-app, try $COUNTER"
+         while [ $COUNTER -lt 5 ]; do
+            echo "Retrying mxt-app reset, try $COUNTER"
+            PATH=`lsusb | grep -i Atmel | busybox cut -d " " -f 1`
+
+            # Simulating reconnect to CN5 port 
+            usbreset $PATH
+            # Send USB packet to Atmel chip to reset itself
             mxt-app --reset
+
+            # Check to see if we need to repeat process
             RESULT=$?
-            if [ $RESULT -eq "0" ]; 
+            if [ $RESULT -eq "0" ];  
                then
+               # stop any additional attempts and display info on Atmel chip
                let COUNTER=100
+               mxt-app -i
             else
+               # try again
                let COUNTER=COUNTER+1
-            fi
+            fi  
          done
-      fi
+      fi  
+      # For Atmel touchscreens we need to disable the egalax touchscreen 
       PROCESS=`ps | grep eGTouchD | busybox tr -s " " | busybox cut -d " " -f 2`
       if [ $PROCESS -gt "0" ];
          then
@@ -48,11 +46,15 @@ if [ $COUNT -ge "1" ];
           echo "Nothing to kill"
       fi
 fi
+
+# EETI touchscreen handling
 COUNT=`lsusb | grep -i egalax | busybox wc -l`
 if [ $COUNT -ge "1" ];
    then
        echo "$COUNT eGalax Touchscreen discovered"
        setprop pdiarm.touchscreen eGalax
 fi
+
+# Indicate completion of touchscreen processing
 PROP="$(getprop pdiarm.touchscreen)"
 echo "Done with touchscreen processing $PROP"
