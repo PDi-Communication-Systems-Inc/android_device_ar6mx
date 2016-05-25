0bdbef627f134fd0badeb68fad72fe780ff71535
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-05-25 15:47:36 -0400

    Okay to put tomdroid back in again

--- ar6mx/ar6mx.mk
+++ ar6mx/ar6mx.mk
@@ -237,6 +237,7 @@ ifneq ($(DAVITA_BUILD),T)
 $(warning Not a Davita build, adding some other packages)
 	PRODUCT_PACKAGES += \
 		k9				\
+		org.tomdroid			\
 		caa-2.1.2-597-us_zeebox		\
 		com.ghostsq.commander		\
 		ghost-sftp-plugin               \
